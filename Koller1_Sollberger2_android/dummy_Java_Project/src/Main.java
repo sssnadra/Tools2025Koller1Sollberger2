677b1583cb4f774070b9902052d862e8cffbf761
Eiswoelfchen <88629140+Eiswoelfchen@users.noreply.github.comm>
2025-05-20 20:55:55 +0200

    changed lines of code in method

--- Koller1_Sollberger2_android/dummy_Java_Project/src/Main.java
+++ Koller1_Sollberger2_android/dummy_Java_Project/src/Main.java
@@ -7,5 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
         }
-    public void thisIsANewMethod() { }
+    public void thisIsANewMethod() {
+        int i = 0;
+        while(i == 0){
+            return;
+        }
     }
+}
